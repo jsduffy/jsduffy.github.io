0d817cf34ebd918f382d499ce940d994b9c450ac
Justin <jsduffy@gmail.com>
2015-05-26 12:47:07 +0300

    fixing slider

--- Egypt_STEM_Schools/js/init.js
+++ Egypt_STEM_Schools/js/init.js
@@ -5,3 +5,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+	//setup slider
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+    
+    //setup modal dialogues
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
